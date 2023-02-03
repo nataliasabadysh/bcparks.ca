095bf7289aa36acca78fa5c3328e6a705f3525df
Natalia Sabadysh <nataliasabadysh@gmail.com>
2023-02-02 22:44:17 -0700

    first commit

--- config/database.js
+++ config/database.js
@@ -4,7 +4,7 @@ module.exports = ({ env }) => ({
     connection: {
       host: env('DATABASE_HOST', 'localhost'),
       port: env.int('DATABASE_PORT', 5432),
-      database: env('DATABASE_NAME', 'stapi4'),
+      database: env('DATABASE_NAME', 'migration_db'),
       user: env('DATABASE_USERNAME', 'postgres'),
       password: env('DATABASE_PASSWORD', 'postgres'),
       schema: env('DATABASE_SCHEMA', 'public'), // Not required
@@ -13,3 +13,5 @@ module.exports = ({ env }) => ({
     debug: true,
   },
 });
+
+// npm run strapi admin:reset-user-password --email="admin@test.test" --password="admiN12345".
